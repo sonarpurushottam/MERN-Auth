d1d3d6c524d07e3005045f5a1529ec34020683c2
Purushottam Sonar <sonarp02@gmail.com>
2024-02-08 22:27:42 +0530

    Add routes and create pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,24 @@
-export default function App() {
-  console.log("shubham");
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import Profile from "./pages/Profile";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/SignIn" element={<SignIn />} />
+          <Route path="/SignUp" element={<SignUp />} />
+          <Route path="/Profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
